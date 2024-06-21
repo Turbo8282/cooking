c8222eaa8995ad1c9803cb5fb0c901249c345447
Alex Wang <alexwang2882@gmail.com>
2024-06-21 14:08:51 +1000

    change environment variable

--- src/components/GPTPrompt.js
+++ src/components/GPTPrompt.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import OpenAI from "openai"; 
 
 // Initialise OpenAI Client with your API Key
-const openai = new OpenAI({ apiKey: '', dangerouslyAllowBrowser: true});
+const openai = new OpenAI({ apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true});
 
 const GPTPrompt = ({ responses }) => {
   const [exportedResponses, setExportedResponses] = useState(null);
